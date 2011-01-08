648cd5dc4427a4d60efae8b4ec4ab12fe1d1a7d9
Curran <curran.kelleher@gmail.com>
2011-01-08 12:23:21 -0500

    added text rendering functionality

---
+++ code/IGF01/src/org/curransoft/igf/test/Test07Text.java
@@ -0,0 +1,39 @@
+package org.curransoft.igf.test;
+
+import java.awt.Font;
+
+import org.curransoft.igf.IGF;
+import org.curransoft.igf.IGFApplicationAdapter;
+
+/**
+ * A simple IGF test which draws a circle.
+ * 
+ * @author curran
+ * 
+ */
+public class Test07Text extends IGFApplicationAdapter {
+	static int appIdCounter = 0;
+	int appId = appIdCounter++;
+
+	int fontID = IGF.INVALID_FONT_ID;
+
+	public static void main(String[] args) {
+		TestUtils.testApplication(new Test07Text(), new Test07Text());
+	}
+
+	public void setup(IGF g) {
+		fontID = g.loadFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+	}
+
+	public void draw(IGF g) {
+		g.background(255);
+		double x = g.getWidth() / 2;
+		double y = g.getHeight() / 2;
+		g.fill(0);
+		g.text("Hello World!", fontID, x, y);
+
+		g.noFill();
+		g.stroke(0);
+		g.strokeWeight(1);
+	}
+}
